a86ef12c2ef217389a0e6f074cd679a9ad586d7a
Hayden Key <Pandathighs@MacBook-Pro-3.local>
2020-06-09 18:05:49 -0400

    added user feedback and tests

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,10 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme, {mount, shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { exportAllDeclaration } from '@babel/types';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const state = {
+  turnData: {
+    books: ['The Shining', 'IT', 'David Copperfeild', 'A Tale of Two Cities', 'Hamlet'],
+    author: {
+      name: 'Charles Dickens',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Charles_Dickens.png',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Wikimedia Commons',
+      books: ['David Copperfeild', 'A Tale of Two Cities']
+    }
+  },
+  highlight: 'none'
+}
 
 describe("Author Quiz", () => {
   it("renders without crashing", () => {
       const div = document.createElement("div");
-      ReactDOM.render(<AuthorQuiz />, div);
+      ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => {}} />, div);
+  });
+
+  describe("when no answer has been selected", ()=> {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => {}}/>);
+    });
+
+    it("should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    });
+  });
+
+  describe("when the wrong answer has been selected", ()=> {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'wrong'}))} onAnswerSelected={() => {}} />);
+    });
+
+    it("should have a red background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('red');
+    });
+  });
+
+  describe("when the correct answer has been selected", ()=> {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'correct'}))} onAnswerSelected={() => {}} />);
+    });
+
+    it("should have a red background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('green');
+    });
+  });
+
+  describe("When the first answer is selected", () => {
+    let wrapper;
+    const handleAnswerSelected = jest.fn();
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={handleAnswerSelected} />);
+      wrapper.find('.answer').first().simulate('click');
+    });
+
+    it("onAnswerSelected should be called", () => {
+      expect(handleAnswerSelected).toHaveBeenCalled();
+    });
+
+    it("should recieve The Shining", () => {
+      expect(handleAnswerSelected).toHaveBeenCalledWith("The Shining");
+    });
   });
 });
